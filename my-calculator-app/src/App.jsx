55e567a7333de5d114c88c4071152b931feec668
Huy Tran <baohuy82.th@gmail.com>
2025-02-23 10:05:17 -0800

    Update App.jsx

--- my-calculator-app/src/App.jsx
+++ my-calculator-app/src/App.jsx
@@ -48,7 +48,10 @@ function SelectPercentage({ children, percentage, onSelect }) {
   return (
     <div>
       <label>{children}</label>
-      <select value={percentage}>
+      <select
+        value={percentage}
+        onChange={(e) => onSelect(Number(e.target.value))}
+      >
         <option value="0">Dissatisfied (0%)</option>
         <option value="5">It was okay (5%)</option>
         <option value="10">It was good (10%)</option>
