8d1de8c0530c8cf65f686de1f8542ed3f6ffd530
Huy Tran <baohuy82.th@gmail.com>
2025-02-23 10:07:44 -0800

    Update App.jsx

--- my-calculator-app/src/App.jsx
+++ my-calculator-app/src/App.jsx
@@ -14,7 +14,7 @@ function TipCalculator() {
   const [percentage1, setPercentage1] = useState(0);
   const [percentage2, setPercentage2] = useState(0);
 
-  const tip = (bill * (percentage1 + percentage2)) / 2 / 100;
+  const tip = bill * ((percentage1 + percentage2) / 2 / 100);
 
   return (
     <div>
